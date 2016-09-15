80001cc0c49622cba9e3da048da1aab6ed716283
BrandonRoyal <BMRoyal@gmail.com>
2016-09-14 17:52:57 -0700

    added build script

---
+++ build.ps1
@@ -0,0 +1,4 @@
+docker build -t brandonroyal/demo_redis:latest ./redis
+docker build -t brandonroyal/demo_web:latest ./web
+docker build -t brandonroyal/demo_api:latest ./api
+docker build -t brandonroyal/demo_nginx:latest ./proxy
