38e18681d92f0c761447afe8ca5433b48a8a4bbb
BrandonRoyal <BMRoyal@gmail.com>
2016-09-14 18:47:18 -0700

    fix

--- run.ps1
+++ run.ps1
@@ -1,4 +1,6 @@
-docker run --name redis  -d brandonroyal/demo_redis:latest
-docker run --name web  -d brandonroyal/demo_web:latest
-docker run --name api  --link redis -d brandonroyal/demo_api:latest
-docker run --name nginx  --link web --link api -p 80:80 -d brandonroyal/demo_nginx:latest
+docker network create default
+
+docker run --name redis --network default -d brandonroyal/demo_redis:latest
+docker run --name web --network default -d brandonroyal/demo_web:latest
+docker run --name api --network --link redis -d brandonroyal/demo_api:latest
+docker run --name nginx --network --link web --link api -p 80:80 -d brandonroyal/demo_nginx:latest
