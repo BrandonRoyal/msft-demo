bc79ed1ac199b9878a0850ee86c41bc5d3ade3c8
BrandonRoyal <BMRoyal@gmail.com>
2016-09-14 18:49:24 -0700

    fixed typo

--- run.ps1
+++ run.ps1
@@ -1,6 +1,6 @@
-docker network create default
+docker network create demo
 
-docker run --name redis --network default -d brandonroyal/demo_redis:latest
-docker run --name web --network default -d brandonroyal/demo_web:latest
-docker run --name api --network --link redis -d brandonroyal/demo_api:latest
-docker run --name nginx --network --link web --link api -p 80:80 -d brandonroyal/demo_nginx:latest
+docker run --name redis --network demo -d brandonroyal/demo_redis:latest
+docker run --name web --network demo -d brandonroyal/demo_web:latest
+docker run --name api --network demo --link redis -d brandonroyal/demo_api:latest
+docker run --name nginx --network demo --link web --link api -p 80:80 -d brandonroyal/demo_nginx:latest
