0f2e765e1b842b7889fa6a6f3b5512db9a8ab608
BrandonRoyal <BMRoyal@gmail.com>
2016-09-14 18:21:11 -0700

    removing always flag to resolving hanging restart

--- run.ps1
+++ run.ps1
@@ -1,4 +1,4 @@
-docker run --name redis --restart always -d brandonroyal/demo_redis:latest
-docker run --name web --restart always -d brandonroyal/demo_web:latest
-docker run --name api --restart always --link redis -d brandonroyal/demo_api:latest
-docker run --name nginx --restart always --link web --link api -p 80:80 -d brandonroyal/demo_nginx:latest
+docker run --name redis  -d brandonroyal/demo_redis:latest
+docker run --name web  -d brandonroyal/demo_web:latest
+docker run --name api  --link redis -d brandonroyal/demo_api:latest
+docker run --name nginx  --link web --link api -p 80:80 -d brandonroyal/demo_nginx:latest
