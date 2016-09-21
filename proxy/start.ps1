e14427ad78a2d76f847ca868ce29dea143ebb46e
BrandonRoyal <BMRoyal@gmail.com>
2016-09-21 13:29:17 -0700

    fixed url prefixes

--- proxy/start.ps1
+++ proxy/start.ps1
@@ -8,8 +8,8 @@ Param (
 )
 
 #ipconfig | Select-String 'Default Gateway' | ForEach-Object { $gateway = $_.Line.Substring(39) }
-$Env:WEB_URI = "http://$WebIp:5000"
-$Env:API_URI = "http://$ApiIp:5000"
+$Env:WEB_URI = "http://$($WebIp):5000"
+$Env:API_URI = "http://$($ApiIp):5000"
 Write-Host "Web URI: $Env:WEB_URI"
 Write-Host "Api URI: $Env:API_URI"
 ./nginx.exe
