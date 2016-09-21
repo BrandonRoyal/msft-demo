c4d64213ca7c0bbe2a1e968efd8b611de31a0484
BrandonRoyal <BMRoyal@gmail.com>
2016-09-21 12:10:31 -0700

    fixed env var

--- api/start.ps1
+++ api/start.ps1
@@ -1,4 +1,4 @@
 ipconfig | Select-String 'Default Gateway' | ForEach-Object { $gateway = $_.Line.Substring(39) }
 $Env:REDIS_IP = $gateway
-Write-Host "Connecting to Default Gateway -> $Env.REDIS_IP"
+Write-Host "Connecting to Default Gateway -> $Env:REDIS_IP"
 dotnet run --server.urls http://0.0.0.0:5000
--- run.ps1
+++ run.ps1
@@ -5,7 +5,7 @@ Write-Host "running demo_web"
 docker run --name web --network default -d -p 5000:5000 brandonroyal/demo_web:latest
 
 Write-Host "running demo_api"
-docker run --name api --network default -p 5000:5000 -d brandonroyal/demo_api:latest
+docker run --name api --network default -p 5001:5000 -d brandonroyal/demo_api:latest
 
 Write-Host "running demo_nginx"
 docker run --name nginx --network default --link web --link api -d brandonroyal/demo_nginx:latest
