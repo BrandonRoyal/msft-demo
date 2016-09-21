3ca333f65de6f87ed3f7a58543db8f331252d53b
BrandonRoyal <BMRoyal@gmail.com>
2016-09-21 12:48:46 -0700

    testing web

--- run.ps1
+++ run.ps1
@@ -5,11 +5,11 @@ Write-Host "redis_ip: $redis_ip"
 Write-Host "------"
 
 Write-Host "running demo_web"
-docker run --name web --network default -d brandonroyal/demo_web:latest
+docker run --name web --network default -p 5000:80 -d brandonroyal/demo_web:latest
 Write-Host "------"
 
 Write-Host "running demo_api"
-docker run --name api --network default -p 5000:80 -d brandonroyal/demo_api:latest powershell ./start.ps1 $redis_ip
+docker run --name api --network default -d brandonroyal/demo_api:latest powershell ./start.ps1 $redis_ip
 Write-Host "------"
 
 Write-Host "skipping demo_nginx"
