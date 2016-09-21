c0e5f6de25a7f495e3317c4ed508e8b36b2333e3
BrandonRoyal <BMRoyal@gmail.com>
2016-09-21 13:09:22 -0700

    added nginx back

---
+++ proxy/start.ps1
@@ -0,0 +1,15 @@
+[CmdletBinding()]
+Param (
+    [Parameter(Mandatory=$TRUE)]
+    [string]$WebIp,
+
+    [Parameter(Mandatory=$TRUE)]
+    [string]$ApiIp
+)
+
+#ipconfig | Select-String 'Default Gateway' | ForEach-Object { $gateway = $_.Line.Substring(39) }
+$Env:WEB_URI = "http://$WebIp:5000"
+$Env:API_URI = "http://$ApiIp:5000"
+Write-Host "Web URI: $Env:WEB_URI"
+Write-Host "Api URI: $Env:API_URI"
+./nginx-1.9.13/nginx.exe
--- run.ps1
+++ run.ps1
@@ -6,12 +6,16 @@ Write-Host "------"
 
 Write-Host "running demo_web"
 docker run --name web --network default -p 5000:80 -d brandonroyal/demo_web:latest
+$web_ip = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" web
+Write-Host "web_ip: $web_ip"
 Write-Host "------"
 
 Write-Host "running demo_api"
 docker run --name api --network default -d brandonroyal/demo_api:latest powershell ./start.ps1 $redis_ip
+$api_ip = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" web
+Write-Host "api_ip: $api_ip"
 Write-Host "------"
 
 Write-Host "skipping demo_nginx"
-#docker run --name nginx --network default -d brandonroyal/demo_nginx:latest
+docker run --name nginx --network default -p 80:80 -d brandonroyal/demo_nginx:latest powershell ./start.ps1 $web_ip $api_ip
 Write-Host "------"
