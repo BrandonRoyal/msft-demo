c8598805504b0b0dd1b2aac9633f3b7868e77b14
BrandonRoyal <BMRoyal@gmail.com>
2016-09-20 17:13:40 -0700

    added config script and new default docker-compose.yml

---
+++ setup-docker-compose.ps1
@@ -0,0 +1,3 @@
+New-Item -Type File c:\ProgramData\docker\config\daemon.json
+Add-Content 'c:\programdata\docker\config\daemon.json' '{ "hosts": ["tcp://0.0.0.0:2375", "npipe://"] }'
+#TODO: set $Env:DOCKER_HOST variable to tcp://localhost:2375
