b824fd806ef4f969a4a032218b740ca09a782f99
BrandonRoyal <BMRoyal@gmail.com>
2016-09-21 12:06:44 -0700

    fixed missing command

--- run.ps1
+++ run.ps1
@@ -1,6 +1,11 @@
-docker run --name redis --network default -d brandonroyal/demo_redis:latest
-$redis_ip = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" test
+Write-Host "running demo_redis"
+docker run --name redis --network default -p 6379:6379 -d brandonroyal/demo_redis:latest
 
-docker run --name web --network default -e "REDIS_IP=$redis_ip" -d brandonroyal/demo_web:latest
-docker run --name api --network default --link redis -d brandonroyal/demo_api:latest
+Write-Host "running demo_web"
+docker run --name web --network default -d -p 5000:5000 brandonroyal/demo_web:latest
+
+Write-Host "running demo_api"
+docker run --name api --network default -p 5000:5000 -d brandonroyal/demo_api:latest
+
+Write-Host "running demo_nginx"
 docker run --name nginx --network default --link web --link api -d brandonroyal/demo_nginx:latest
