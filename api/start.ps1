2767a3e44793505ed5414690f74d7eb5d25bfce0
BrandonRoyal <BMRoyal@gmail.com>
2016-09-21 12:32:09 -0700

    updating run

--- api/start.ps1
+++ api/start.ps1
@@ -1,4 +1,9 @@
-ipconfig | Select-String 'Default Gateway' | ForEach-Object { $gateway = $_.Line.Substring(39) }
-$Env:REDIS_IP = $gateway
+Param (
+    [Parameter(Mandatory=$TRUE)]
+    [string]$RedisIp
+)
+
+#ipconfig | Select-String 'Default Gateway' | ForEach-Object { $gateway = $_.Line.Substring(39) }
+$Env:REDIS_IP = $RedisIp
 Write-Host "Connecting to Default Gateway -> $Env:REDIS_IP"
 dotnet run --server.urls http://0.0.0.0:5000
--- run.ps1
+++ run.ps1
@@ -1,11 +1,17 @@
 Write-Host "running demo_redis"
-docker run --name redis --network default -p 6379:6379 -d brandonroyal/demo_redis:latest
+docker run --name redis --network default -d brandonroyal/demo_redis:latest
+$redis_ip = docker inspect -f "{{ .NetworkSettings.Networks.nat.IPAddress }}" redis
+Write-Host "redis_ip: $redis_ip"
+Write-Host "------"
 
 Write-Host "running demo_web"
-docker run --name web --network default -d -p 5000:5000 brandonroyal/demo_web:latest
+docker run --name web --network default -d brandonroyal/demo_web:latest
+Write-Host "------"
 
 Write-Host "running demo_api"
-docker run --name api --network default -p 5001:5000 -d brandonroyal/demo_api:latest
+docker run --name api --network default -d brandonroyal/demo_api:latest powershell ./start.ps1 $redis_ip
+Write-Host "------"
 
-Write-Host "running demo_nginx"
-docker run --name nginx --network default --link web --link api -d brandonroyal/demo_nginx:latest
+Write-Host "skipping demo_nginx"
+#docker run --name nginx --network default -d brandonroyal/demo_nginx:latest
+Write-Host "------"
